92e757c76035e3ecf1a5557039a88dbe1a355ef9
Soaif Ikbal Mondal <soaif0912@gmail.com>
2024-09-03 00:18:05 +0530

    learning classes and objects

---
+++ OOPS/TwoClass/Main.java
@@ -0,0 +1,6 @@
+package TwoClass;
+
+public class Main {
+    String name = "Soaif Ikbal Mondal";
+    int age = 24 ;
+}
---
+++ OOPS/TwoClass/Second.java
@@ -0,0 +1,9 @@
+package TwoClass;
+
+public class Second {
+    public static void main(String[] args){
+        Main MySelf = new Main();
+        System.out.println(MySelf.name);
+        System.out.println(MySelf.age);
+    }
+}
