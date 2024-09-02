5d01a81c9146df1e66d67bf94279ff509ed8e2f1
Soaif Ikbal Mondal <soaif0912@gmail.com>
2024-09-03 00:09:14 +0530

    adding oops file

---
+++ OOPS/ClassesObjects.java
@@ -0,0 +1,8 @@
+class ClassesObjects{
+    String name ="Soaif Ikbal";
+    public static void main(String[] args){
+        ClassesObjects a = new ClassesObjects();
+        System.out.println(a.name);
+    }
+
+}
