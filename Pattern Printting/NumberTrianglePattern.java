5ee307b8f854735cb2d5dbf12b945eca5a067ec1
Soaif Ikbal Mondal <soaif0912@gmail.com>
2024-09-03 23:45:20 +0530

    Adding NumberTrianglePattern

---
+++ Pattern Printting/NumberTrianglePattern.java
@@ -0,0 +1,17 @@
+public class NumberTrianglePattern {
+    public static void printPattern(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int n = 6;
+        printPattern(n);
+    }
+}
