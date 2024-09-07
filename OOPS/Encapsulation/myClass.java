bdfba928d6a86320ec610d13512b654f1d18171a
Soaif Ikbal Mondal <soaif0912@gmail.com>
2024-09-07 11:43:09 +0530

    Adding encapsulation

---
+++ OOPS/Encapsulation/myClass.java
@@ -0,0 +1,10 @@
+package Encapsulation;
+
+public class myClass {
+    
+    public static void main(String[] args){
+        Person Soaif = new Person();
+        Soaif.setName("Soaif Ikbal Mondal");
+        System.out.println("Name is " + Soaif.getName());
+    }
+}
