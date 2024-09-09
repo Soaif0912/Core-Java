2109cfd05d1b915d7f9a57b84cbfd77721db81c2
Soaif Ikbal Mondal <soaif0912@gmail.com>
2024-09-10 00:16:00 +0530

    adding linkedList

---
+++ OOPS/LinkedList/Main.java
@@ -0,0 +1,15 @@
+package LinkedList;
+import java.util.LinkedList;
+
+public class Main {
+        public static void main(String[] args){
+
+        LinkedList<String> cars = new LinkedList<String>();
+
+        cars.add("Honda");
+        cars.add("suzuki");
+        cars.add("mahindra");
+
+        System.out.println(cars);
+    }
+}
