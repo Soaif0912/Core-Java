05be5468803cc6d509b35440ad8c800ef6faf21a
Soaif Ikbal Mondal <soaif0912@gmail.com>
2024-09-12 00:30:20 +0530

    Adding string to char type array

---
+++ Questions/StringArray.java
@@ -0,0 +1,14 @@
+package Questions;
+
+public class StringArray {
+    public static void main(String[] args){
+
+        String name = "Soaif";
+
+        char[] myArray = name.toCharArray();
+
+        for(char i : myArray){
+            System.out.println(i);
+        }
+    }
+}
