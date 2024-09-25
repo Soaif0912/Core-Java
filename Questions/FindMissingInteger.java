a84ac6149f757ec19c76bb20ebe9df5a6973a99a
Soaif Ikbal Mondal <soaif0912@gmail.com>
2024-09-25 23:58:29 +0530

    finding Missing Integer

---
+++ Questions/FindMissingInteger.java
@@ -0,0 +1,27 @@
+package Questions;
+
+public class FindMissingInteger {
+    public static int missingNumber(int n, int[] arr)
+    {
+        int[] hash = new int[n + 1];
+
+        for (int i = 0; i < n - 1; i++) {
+            hash[arr[i]]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (hash[i] == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = { 1, 2, 3, 5 };
+        int n = 5;
+        System.out.println(missingNumber(n, arr));
+    }
+}
