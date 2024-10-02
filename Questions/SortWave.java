9a709c001944f13088d28d67ff5ede70b2168c0d
Soaif Ikbal Mondal <soaif0912@gmail.com>
2024-10-02 23:57:46 +0530

    Sort array in wave pattern

---
+++ Questions/SortWave.java
@@ -0,0 +1,32 @@
+package Questions;
+
+public class SortWave {
+  
+    void swap(int arr[], int a, int b)
+    {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    void sortInWave(int arr[], int n)
+    {
+        for(int i = 0; i < n-1; i+=2){
+            //swap odd and even positions
+            if(i > 0 && arr[i - 1] > arr[i])
+              swap(arr, i, i-1);
+              if(i < n-1 && arr[i + 1] > arr[i])
+              swap(arr, i, i+1);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        SortWave ob = new SortWave();
+        int arr[] = {10, 90, 49, 2, 1, 5, 23};
+        int n = arr.length;
+        ob.sortInWave(arr, n);
+        for (int i : arr)
+            System.out.print(i+" ");
+    }
+}
