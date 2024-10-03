e0339b6ed06d8c1d6d2434fcc82c97d4735c755f
Soaif Ikbal Mondal <soaif0912@gmail.com>
2024-10-04 00:42:06 +0530

    Is array subset of other array.

---
+++ Questions/ArraySubsetOfArray.java
@@ -0,0 +1,37 @@
+package Questions;
+
+
+public class ArraySubsetOfArray {
+
+    public static boolean isSubset(int[] arr1, int[] arr2, int m, int n) {
+        // Iterate over each element in the second array
+        for (int i = 0; i < n; i++) {
+            boolean found = false;
+            // Check if the element exists in the first array
+            for (int j = 0; j < m; j++) {
+                if (arr2[i] == arr1[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            // If any element is not found, return false
+            if (!found) return false;
+        }
+        // If all elements are found, return true
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {11, 1, 13, 21, 3, 7};
+        int[] arr2 = {11, 3, 7, 1};
+        int m = arr1.length;
+        int n = arr2.length;
+
+        if (isSubset(arr1, arr2, m, n)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+    
+}
